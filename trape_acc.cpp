f658703be8cb1eb661b144d2daefbf7291fdd4e8
Hosogaya <hosogaya.hirotaka@b.mbox.nagoya-u.ac.jp>
2021-09-07 13:50:58 +0900

    change structure of trape_acc and add function to deceleration immediately

--- trape_acc.cpp
+++ trape_acc.cpp
@@ -1,12 +1,24 @@
 #include "trape_acc.h"
 #include <fstream>
 #include <iostream>
+#include <array>
 void TrapeAcc::reset(const double dist, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark){
-    this->calParame(dist, acc_s, vel_s, acc_m, vel_m, jark);
-    while(std::abs(dist) < std::abs(this->dist_a1_ + this->dist_a2_ + this->dist_af_ + this->dist_d1_ + this->dist_d2_ + this->dist_df_)){
-        this->calParame(dist, acc_s, vel_s, acc_m, this->vel_m_ * 0.8, jark);
+    // this->calParame(dist, acc_s, vel_s, acc_m, vel_m, jark);
+    // while(std::abs(dist) < std::abs(this->dist_a1_ + this->dist_a2_ + this->dist_af_ + this->dist_d1_ + this->dist_d2_ + this->dist_df_)){
+    //     this->calParame(dist, acc_s, vel_s, acc_m, this->vel_m_ * 0.8, jark);
+    // }
+    double vel_mm = std::abs(vel_m) * this->sign(dist);
+    this->calParame2(this->accel_, acc_s, vel_s, acc_m, vel_mm, jark);
+    this->calParame2(this->decel_, 0.0, vel_mm, acc_m, 0.0, jark);
+    while(std::abs(dist) < std::abs(accel_.dist_1 + accel_.dist_2 + accel_.dist_f + decel_.dist_1 + decel_.dist_2 + decel_.dist_f)){
+        vel_mm = vel_mm * 0.8;
+        this->calParame2(this->accel_, acc_s, vel_s, acc_m, vel_mm, jark);
+        this->calParame2(this->decel_, 0.0, vel_mm, acc_m, 0.0, jark);
     }
-    this->print();
+    this->t_ = 0.0;
+    // this->print();
+    this->print(accel_);
+    this->print(decel_);
 }
 
 void TrapeAcc::calParame(const double dist, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark){
@@ -75,6 +87,37 @@ void TrapeAcc::calParame(const double dist, const double acc_s, const double vel
     this->dist_im_ = dist - (this->dist_a1_ + this->dist_a2_ + this->dist_af_) - (this->dist_d1_ + this->dist_d2_ + this->dist_df_);
 }
 
+// pre calcurate vel_m
+void TrapeAcc::calParame2(Params& param, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark){
+    param.vel_m = vel_m;
+    param.acc_m = std::abs(acc_m) * this->sign(param.vel_m - vel_s);
+    param.jark_1 = std::abs(jark) * this->sign(param.acc_m - acc_s);
+    param.jark_f = -std::abs(jark) * this->sign(param.acc_m);
+
+    double delta_vel_1 = (pow(param.acc_m, 2) - pow(acc_s, 2)) / (2 * param.jark_1);
+    double delta_vel_f = 3 * pow(param.acc_m, 2) / (2 * param.jark_f);
+    bool is_acc_const = true;
+    if (std::abs(delta_vel_1 + delta_vel_f) > std::abs(param.vel_m - vel_s)){
+        param.acc_m = std::sqrt(std::abs((2 * param.jark_1 * param.jark_f) * (param.vel_m - vel_s + pow(acc_s, 2) / param.jark_1) / (3 * param.jark_1 + param.jark_f))) * this->sign(param.acc_m);
+        is_acc_const = false;
+    }
+    param.time_1 = std::abs((param.acc_m - acc_s) / param.jark_1);
+    param.time_f = std::abs((param.vel_m - vel_s - acc_s * param.time_1 - pow(param.acc_m - acc_s, 2) / (2 * param.jark_1) - pow(param.acc_m, 2) / (2 * param.jark_f)) / param.acc_m) + param.time_1;
+    if (is_acc_const){
+        param.time_2 = param.time_f - std::abs(param.acc_m / param.jark_f);
+    }
+    else{
+        param.time_2 = param.time_1;
+    }
+    double vel_1 = delta_vel_1 + vel_s;
+    double vel_2 = param.acc_m * (param.time_2 - param.time_1) + vel_1;
+
+    param.dist_1 = acc_s * pow(param.time_1, 2) * 0.5 + param.jark_1 * pow(param.time_1, 3) / 6 + vel_s * param.time_1;
+    param.dist_2 = param.acc_m * pow(param.time_2 - param.time_1, 2) * 0.5 + vel_1 * (param.time_2 - param.time_1);
+    param.dist_f = param.acc_m * pow(param.time_f - param.time_2, 2) * 0.5 + param.jark_f *pow(param.time_f - param.time_2, 3) / 6 + vel_2 * (param.time_f - param.time_2);
+}
+
+
 bool TrapeAcc::calVelAcc(const double dist, const double period, const double vel, const double acc,  double& vel_next, double& acc_next){
     this->t_ += period;
 
@@ -115,6 +158,66 @@ bool TrapeAcc::calVelAcc(const double dist, const double period, const double ve
     return true;
 }
 
+bool TrapeAcc::calVelAcc2(const double dist, const double period, const double vel, const double acc,  double& vel_next, double& acc_next){
+    if (state_ == Acc){
+        this->t_ += period;
+        if (t_ <= accel_.time_f) this->update(accel_, t_, period, acc, vel, acc_next, vel_next);
+        else {
+            state_ = Const;
+            t_ = 0.0;
+            std::cout << "Switch to const phase" << std::endl;
+        }
+    }
+    if (state_ == Const){
+        if (std::abs(dist) > std::abs(decel_.dist_1 + decel_.dist_2 + decel_.dist_f)){
+            vel_next = accel_.vel_m;
+            acc_next = 0.0;
+        }
+        else{
+            state_ = Dec;
+            t_ = 0.0;
+            std::cout << "Switch to Deceleration phase" <<std::endl;
+        }
+    }
+    if (state_ == Dec){
+        this->t_ += period;
+        if (t_ <= decel_.time_f) this->update(decel_, t_, period, acc, vel, acc_next, vel_next);
+        else {
+            acc_next = 0.0;
+            vel_next = 0.0;
+            return false;
+        }
+    }
+    return true;
+}
+
+void TrapeAcc::update(Params& param, double t, double period, double acc, double vel, double& acc_next, double& vel_next){
+    if (t > param.time_2){
+        acc_next = acc + param.jark_f * period;
+        vel_next = vel + acc * period + param.jark_f * pow(period, 2.0) * 0.5;
+    }
+    else if (t > param.time_1){
+        acc_next = param.acc_m;
+        vel_next = vel + acc * period;
+    }
+    else {
+        acc_next = acc + param.jark_1 * period;
+        vel_next = vel + acc * period + param.jark_1 * pow(period, 2.0) * 0.5;
+    }
+}
+/**
+ * @brief create acceleration profile to goal velocity regardless of the distance
+ * @param jark : constant jark
+ * @param acc_s : current acceleration
+ * @param vel_s : current velocity
+ * @param acc_m : maximum amplitude of acceleration
+ * @param vel_m : goal velocity
+*/
+void TrapeAcc::Decel(const double jark, const double acc_s, const double vel_s, const double acc_m, const double vel_m){
+    state_ = Dec;
+    t_ = 0.0;
+    this->calParame2(decel_, acc_s, vel_s, acc_m, vel_m, jark);
+}
 void TrapeAcc::print(){
     std::cout << this->time1a_ << ", " << this->time2a_ << ", " << this->timefa_ << std::endl;
     std::cout << this->time1d_ << ", " << this->time2d_ << ", " << this->timefd_ << std::endl;
@@ -125,11 +228,18 @@ void TrapeAcc::print(){
     std::cout << this->acc_ma_  << ", " << this->acc_md_ << std::endl;
 }
 
+void TrapeAcc::print(Params& param){
+    std::cout << param.time_1 << ", " << param.time_2 << ", " << param.time_f << std::endl;
+    std::cout << param.dist_1 << ", " << param.dist_2 << ", " << param.dist_f << std::endl;
+    std::cout << param.jark_1 << ", " << param.jark_f << std::endl;
+    std::cout << param.acc_m << ", " << param.vel_m << std::endl;
+}
+
 void globalRotation(std::array<double, 3>& src, const std::array<double, 3>& pose, std::array<double, 3>& dist){
     double temp = src[2] * cos(pose[0]) - src[0] * sin(pose[0]);
     dist[0] = src[0] * cos(pose[0]) + src[2] * sin(pose[0]);
     dist[2] = temp * cos(pose[1]) + src[1] * sin(pose[1]);
-    dist[1] = src * cos(pose[1]) - temp * sin(pose[1]);
+    dist[1] = src[1] * cos(pose[1]) - temp * sin(pose[1]);
 }
 
 
@@ -157,6 +267,9 @@ int main(){
     bool break_flag_x = false;
     bool break_flag_y = false;
 
+    bool reset_flag = false;
+    bool deceleration_flag = true;
+
     std::ofstream ofs("./out.csv");
     if (!ofs){
         std::cout << "ファイルが開けませんでした。" << std::endl;
@@ -169,8 +282,16 @@ int main(){
     pro_x.reset(dist_x, acc_s, vel_s, acc_m, vel_m, jark);
     pro_y.reset(dist_y, acc_s, vel_s, acc_m, vel_m, jark);
     while (true){
-        if (break_flag_x == false) pro_x.calVelAcc(dist_x, period, vel_x, acc_x, vel_nx, acc_nx);
-        if (break_flag_y == false) pro_y.calVelAcc(dist_y, period, vel_y, acc_y, vel_ny, acc_ny);
+        if (i > 800 and deceleration_flag){
+            pro_x.Decel(jark, acc_x, vel_x, acc_m, 0.0);
+            deceleration_flag = false;
+        }
+        if (break_flag_x == false) {
+            if(pro_x.calVelAcc2(dist_x, period, vel_x, acc_x, vel_nx, acc_nx) == false) break_flag_x = true;
+        }
+        if (break_flag_y == false) {
+            if(pro_y.calVelAcc2(dist_y, period, vel_y, acc_y, vel_ny, acc_ny) == false) break_flag_y = true;
+        }
         ofs << i * period << ", ";
         ofs << dist_xi - dist_x << ", ";
         ofs << dist_yi - dist_y << ", ";
@@ -179,12 +300,12 @@ int main(){
         ofs << acc_x << ", ";
         ofs << acc_y <<std::endl;
         if (break_flag_x and break_flag_y) break;
-        if (dist_x * (dist_x - vel_x*period - acc_x * pow(period, 2.0) * 0.5) <= 0.0){
-            break_flag_x = true;
-        }
-        if (dist_y * (dist_y - vel_y*period - acc_y * pow(period, 2.0) * 0.5) <= 0.0){
-            break_flag_y = true;
-        }
+        // if (dist_x * (dist_x - vel_x*period - acc_x * pow(period, 2.0) * 0.5) <= 0.0){
+        //     break_flag_x = true;
+        // }
+        // if (dist_y * (dist_y - vel_y*period - acc_y * pow(period, 2.0) * 0.5) <= 0.0){
+        //     break_flag_y = true;
+        // }
         
         dist_x = dist_x - vel_x * period - acc_x * pow(period, 2.0) * 0.5;
         dist_y = dist_y - vel_y * period - acc_y * pow(period, 2.0) * 0.5;
--- trape_acc.h
+++ trape_acc.h
@@ -6,12 +6,32 @@ class TrapeAcc{
     public:
         TrapeAcc(){};
         ~TrapeAcc(){};
+        typedef struct{
+            double jark_1;
+            double jark_f;
+            double acc_m;
+            double vel_m;
+            double time_1;
+            double time_2;
+            double time_f;
+            double dist_1;
+            double dist_2;
+            double dist_f;
+        }Params;
+
         void reset(const double dist, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark);
         void calParame(const double dist, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark);
         bool calVelAcc(const double dist, const double period, const double vel, const double acc,  double& vel_next, double& acc_next);
+        bool calVelAcc2(const double dist, const double period, const double vel, const double acc,  double& vel_next, double& acc_next);
         void print();
+        void print(Params& param);
+        void Decel(const double jark, const double acc_s, const double vel_s, const double acc_m, const double vel_m);
     private:
+
         double sign(double val){return val >= 0 ? 1.0 : -1.0;}
+        void calParame2(Params& param, const double acc_s, const double vel_s, const double acc_m, const double vel_m, const double jark);
+        void calVelAcc3(Params& param, double& acc_next, double vel_next);
+        void update(Params& param, double t, double period, double acc, double vel, double& acc_next, double& vel_next);
     
     private:
         double jark_1a_;
@@ -38,4 +58,11 @@ class TrapeAcc{
         double dist_d2_;
         double dist_df_;
         double dist_im_;
+
+        Params accel_;
+        Params decel_;
+
+        enum{
+            Acc, Const, Dec
+        } state_ = Acc;
 };
